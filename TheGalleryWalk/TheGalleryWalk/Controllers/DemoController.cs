f12af55a91bccdd5c51bdd17624264d4c052fe08
cenck09 <cenck09@gmail.com>
2016-02-15 20:23:52 -0500

    Added Start of project
    A starter project with a basic mobile friendly menu that collapses into
    a drop down hamburger menu for small widths

---
+++ TheGalleryWalk/TheGalleryWalk/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheGalleryWalk.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+     
+   
+    public ActionResult Demo()
+    {
+        ViewBag.Message = "A Demo page.";
+
+        return View();
+    }
+
+   
+    }
+
+}
