df0277b41ba7d69412d725402d62fd224d13362d
cenck09 <cenck09@gmail.com>
2016-03-17 16:56:17 -0400

    Changed style & added cod drop menu

--- TheGalleryWalk/TheGalleryWalk/Controllers/OwnedGalleriesController.cs
+++ TheGalleryWalk/TheGalleryWalk/Controllers/OwnedGalleriesController.cs
@@ -23,6 +23,23 @@ namespace TheGalleryWalk.Controllers
             {
                 var ownerData = new GalleryOwnerData();
                 ownerData.EmailAddress = user.Email;
+
+                ownerData.galleries = new GalleryEntity[5];
+                ownerData.galleries[0] = new GalleryEntity();
+                ownerData.galleries[0].Name = "G0";
+
+                ownerData.galleries[1] = new GalleryEntity();
+                ownerData.galleries[1].Name = "G1";
+
+                ownerData.galleries[2] = new GalleryEntity();
+                ownerData.galleries[2].Name = "G2";
+
+                ownerData.galleries[3] = new GalleryEntity();
+                ownerData.galleries[3].Name = "G3";
+
+                ownerData.galleries[4] = new GalleryEntity();
+                ownerData.galleries[4].Name = "G4";
+
                 return View("../OwnedGalleries/OwnedGalleries", "_LayoutLoggedIn", ownerData);
             }
             else
